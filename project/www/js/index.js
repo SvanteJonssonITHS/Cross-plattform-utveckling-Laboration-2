ae827a0c21645597e24a7bacf461edccd772873a
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-06-03 19:30:23 +0200

    Create function `createCity`

--- project/www/js/index.js
+++ project/www/js/index.js
@@ -1,12 +1,23 @@
 document.addEventListener('deviceready', main, false);
 
 async function main() {
-	console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-	console.log(alert(JSON.stringify(await getRandomCity())));
+	const city = await getRandomCity();
+	const cityElement = createCity(city);
+	document.querySelector('#city-wrapper').appendChild(cityElement);
 }
 
 async function getRandomCity() {
 	const request = await fetch('https://avancera.app/cities');
 	const response = await request.json();
 	return response ? response[Math.floor(Math.random() * response.length)] : null;
+}
+
+function createCity(city) {
+	const cityElement = document.createElement('div');
+	cityElement.classList.add('city');
+	cityElement.innerHTML = `
+		<h2>${city.name}</h2>
+		<p>Invånare: ${city.population}</p>
+	`;
+	return cityElement;
 }
