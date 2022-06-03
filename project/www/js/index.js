a72c813d52fa8bcc875145d511ff986eae0a96fc
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-06-03 19:16:21 +0200

    Create function `getRandomCity`

--- project/www/js/index.js
+++ project/www/js/index.js
@@ -1,5 +1,12 @@
 document.addEventListener('deviceready', main, false);
 
-function main() {
+async function main() {
 	console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+	console.log(alert(JSON.stringify(await getRandomCity())));
+}
+
+async function getRandomCity() {
+	const request = await fetch('https://avancera.app/cities');
+	const response = await request.json();
+	return response ? response[Math.floor(Math.random() * response.length)] : null;
 }
